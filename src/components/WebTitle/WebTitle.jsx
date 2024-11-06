f01e3e0bdd6e55810a987ea785c29fb8184f75d0
neelarani <neela9624@gmail.com>
2024-11-06 21:27:47 +0600

    added title fev

---
+++ src/components/WebTitle/WebTitle.jsx
@@ -0,0 +1,12 @@
+// import React, { useEffect } from 'react';
+
+// const WebTitle = ({productTitle}) => {
+//   useEffect(() => {
+//     document.title = productTitle? `${productTitle} : 'Gadget hub'`
+//   }, [productTitle])
+//   return (
+//    <></>
+//   );
+// };
+
+// export default WebTitle;
